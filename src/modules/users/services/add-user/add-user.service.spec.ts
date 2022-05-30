e67a6be2e79e7fde0fa129e8ac1f7e8541860a0c
William Koller <williamkoller404@gmail.com>
2022-05-30 00:21:33 -0300

    test: should be called repository with correct params

--- src/modules/users/services/add-user/add-user.service.spec.ts
+++ src/modules/users/services/add-user/add-user.service.spec.ts
@@ -2,14 +2,17 @@ import { AddUserService } from './add-user.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersRepository } from '@/modules/users/repositories/users.repository';
 import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter/bcrypt-adapter';
+import { UserEntity } from '@/infra/typeorm/entities';
 
 describe('AddUserService', () => {
   let addUserService: AddUserService;
   let usersRepo: UsersRepository;
+  let mockData: UserEntity;
 
   beforeEach(async () => {
     const usersRepositoryMock = {
       add: jest.fn(),
+      findUserByEmail: jest.fn(),
     };
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -24,9 +27,24 @@ describe('AddUserService', () => {
 
     addUserService = module.get<AddUserService>(AddUserService);
     usersRepo = module.get<UsersRepository>(UsersRepository);
+    mockData = {
+      id: 'any_id',
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'valid@mail.com',
+      password: 'any_password',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
   });
 
   it('should be defined', () => {
     expect(addUserService).toBeDefined();
+  });
+
+  it('should be called repository with correct params', async () => {
+    (usersRepo.add as jest.Mock).mockReturnValue(mockData);
+
+    expect(await addUserService.execute(mockData)).toEqual(mockData);
   });
 });
