c772ee234458314ad477d755656cdfa29cd9f6be
William Koller <williamkoller404@gmail.com>
2022-05-29 22:31:37 -0300

    chore: add export find-all-users.repository

--- src/data/protocols/db/users/index.ts
+++ src/data/protocols/db/users/index.ts
@@ -2,3 +2,4 @@ export * from './add-user.repository';
 export * from './find-user-by-name.repository';
 export * from './find-user-by-email.repository';
 export * from './find-user-by-id.repository';
+export * from './find-all-users.repository';
