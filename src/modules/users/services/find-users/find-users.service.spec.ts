4cfc2ee39979c85f2979cddf9ed4629f9ddf2578
William Koller <williamkoller404@gmail.com>
2022-05-30 00:06:09 -0300

    test: should be return repository return

--- src/modules/users/services/find-users/find-users.service.spec.ts
+++ src/modules/users/services/find-users/find-users.service.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersRepository } from '@/modules/users/repositories/users.repository';
 import { FindUsersService } from './find-users.service';
+import { UserEntity } from '@/infra/typeorm/entities';
 
 describe('FindUsersService', () => {
   let findUsersService: FindUsersService;
   let usersRepo: UsersRepository;
+  let mockData;
 
   beforeEach(async () => {
     const usersRepositoryMock = {
@@ -22,9 +24,25 @@ describe('FindUsersService', () => {
 
     findUsersService = module.get<FindUsersService>(FindUsersService);
     usersRepo = module.get<UsersRepository>(UsersRepository);
+    mockData = [
+      {
+        id: 'any_id',
+        name: 'any_name',
+        surname: 'any_surname',
+        email: 'valid@mail.com',
+        password: 'any_password',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ] as UserEntity[];
   });
 
   it('should be defined', () => {
     expect(findUsersService).toBeDefined();
+  });
+
+  it('should be return repository return', async () => {
+    (usersRepo.findAll as jest.Mock).mockReturnValue(mockData);
+    expect(await usersRepo.findAll()).toEqual(mockData);
   });
 });
