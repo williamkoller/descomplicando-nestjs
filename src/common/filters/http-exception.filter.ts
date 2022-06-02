d58a68c4ff2694f00c8c95ce90f9a063e823a726
William Koller <williamkoller404@gmail.com>
2022-06-02 20:57:55 -0300

    feat: add filters, interceptors and pipes

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,38 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private readonly logger = new Logger(HttpExceptionFilter.name)) {}
+  catch(exception: HttpException, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : exception;
+
+    this.logger.error(
+      `Http Status: ${status} Error Message: ${JSON.stringify(message)}`,
+    );
+
+    response.status(status).json({
+      method: request.method,
+      path: request.url,
+      details: message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,18 @@ import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { swaggerConfig } from './docs/swagger.doc';
+import { LoggingInterceptor, TimeoutInterceptor } from './common/interceptors';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.useGlobalInterceptors(new TimeoutInterceptor(), new LoggingInterceptor());
 
   app.setGlobalPrefix('api/v1');
 
