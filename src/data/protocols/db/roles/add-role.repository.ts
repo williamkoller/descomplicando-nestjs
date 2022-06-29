498c3940f84db71ec6c49d9f93aa4354ed9cdcfd
William Koller <williamkoller404@gmail.com>
2022-06-29 00:52:51 -0300

    fx: add ';'

--- src/data/protocols/db/roles/add-role.repository.ts
+++ src/data/protocols/db/roles/add-role.repository.ts
@@ -1,5 +1,5 @@
 import { RoleEntity } from '@/infra/typeorm/entities/role-entity/role-entity';
-import { AddRoleDto } from '@/modules/roles/dtos/add-role/add-role.dto'
+import { AddRoleDto } from '@/modules/roles/dtos/add-role/add-role.dto';
 
 export interface AddRoleRepository {
   add: (data: AddRoleDto) => Promise<RoleEntity>;
